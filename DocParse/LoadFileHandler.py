ab016fcc1f0b5a0086ae51eec6f9bb35f7b69150
unknown <aaronhannat@gmail.com>
2018-06-17 00:12:54 +0800

    [add little thing]

--- DocParse/LoadFileHandler.py
+++ DocParse/LoadFileHandler.py
@@ -1,4 +1,13 @@
+import re
 class FileContainer:
-    def __init__(self, sourcePath, format):
+    def __init__(self, sourcePath, format=0):
+        if (format == 0):
+            fileType = re.search('\.[^\.]+$', sourcePath)
+            print(fileType)
         self.filePath = sourcePath
         self.format   = format
+
+    def openFile(self):
+        self.content = open(self.filePath, "r")
+        for line in self.content.readlines():
+            print(line, end='')
---
+++ testMain.py
@@ -0,0 +1,4 @@
+import DocParse.LoadFileHandler as FileHandler
+
+test = FileHandler.FileContainer("C:\\Users\\User\\Downloads\\test.txt", 0 )
+test.openFile()
