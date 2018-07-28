18b3d3a39971789e7ced62594c6eae45b04dd95e
unknown <aaronhannat@gmail.com>
2018-07-28 22:22:57 +0800

    [add commend]

--- AutoPasteMain.py
+++ AutoPasteMain.py
@@ -1,10 +1,14 @@
 import sys
 import DocParse.LoadFileHandler as fileHandler
 
+
 def MainRoutine():
     print('test')
+    container = fileHandler.FileContainer(sys.argv[1])
+    container.openFile()
+    '''
     for input in sys.argv:
         print(input)
         fileHandler.forTestFunction(input)
-
+    '''
 MainRoutine()
--- DocParse/LoadFileHandler.py
+++ DocParse/LoadFileHandler.py
@@ -1,14 +1,20 @@
 import re
 
 def forTestFunction(fileName):
+    print(fileName)
     fileType = re.search(r'\.[^\.]*$', fileName)
     print(fileType.group(0))
+'''==============================================================================
+class: FileContainer
+function:
+Declare a class object to keep a file parsing record in memory.
+The purse of declare an object is to prevent file size too large, and is hard to load the whole file at once.
 
+==============================================================================s'''
 class FileContainer:
     def __init__(self, sourcePath, format=0):
         if (format == 0):
             fileType = re.search('\.[^\.]+$', sourcePath)
-            print(fileType)
         self.filePath = sourcePath
         self.format   = format
 
